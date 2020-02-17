ab38565b8389bb7ed4d9cfaae094a7811b06dad6
League Student <league@iMac-109.attlocal.net>
2020-02-16 17:28:14 -0800

    i finished Guest book

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,19 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	
+	
+	
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +22,44 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	ArrayList<String> names = new ArrayList<String>();
+	
+		
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	JButton button = new JButton();
+	JButton otherButton = new JButton();
+	
+	public void runner() {
+		
+		panel.add(label);
+		panel.add(button);
+		panel.add(otherButton);
+		button.setText("Add names to list");
+		otherButton.setText("View Names");
+		button.addActionListener(this);
+		otherButton.addActionListener(this);
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.pack();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton pressed = (JButton) e.getSource();
+		if (button == pressed) {
+			String hi = JOptionPane.showInputDialog("Enter a name");
+			names.add(hi);
+			
+		}
+		if (otherButton == pressed) {
+			
+			for (int i = 0; i < names.size(); i++) {
+				JOptionPane.showMessageDialog(null, "Guest "+i+": "+names.get(i));
+			}
+			
+		}
+		
+	}
 }
---
+++ src/intro_to_array_lists/GuestBook_Runner.java
@@ -0,0 +1,12 @@
+package intro_to_array_lists;
+
+public class GuestBook_Runner {
+
+public static void main(String[] args) {
+	new GuestBook().runner();
+
+}
+	
+
+}
+
